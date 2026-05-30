c2aecfbc245580c6b4f7965a59d1abe0659f3242
SHsiam <mdshiam4@gmail.com>
2026-05-30 16:56:28 +0100

    fix server.ts for Vercel serverless

--- src/server.ts
+++ src/server.ts
@@ -8,75 +8,73 @@ import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
 
 let server: Server;
 
-
 const startServer = async () => {
-    try {
-        await mongoose.connect(envVars.DB_URL)
+  try {
+    await mongoose.connect(envVars.DB_URL);
 
-        console.log("Connected to DB!!");
+    console.log("Connected to DB!!");
 
-        server = app.listen(envVars.PORT, () => {
-            console.log(`Server is listening to port ${envVars.PORT}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is listening to port ${envVars.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 (async () => {
-    await connectRedis()
-    await startServer()
-    await seedSuperAdmin()
-})()
+  await connectRedis();
+  await startServer();
+  await seedSuperAdmin();
+})();
 
 process.on("SIGTERM", () => {
-    console.log("SIGTERM signal recieved... Server shutting down..");
+  console.log("SIGTERM signal recieved... Server shutting down..");
 
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
-})
+  process.exit(1);
+});
 
 process.on("SIGINT", () => {
-    console.log("SIGINT signal recieved... Server shutting down..");
+  console.log("SIGINT signal recieved... Server shutting down..");
 
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
-})
-
+  process.exit(1);
+});
 
 process.on("unhandledRejection", (err) => {
-    console.log("Unhandled Rejecttion detected... Server shutting down..", err);
+  console.log("Unhandled Rejecttion detected... Server shutting down..", err);
 
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
-})
+  process.exit(1);
+});
 
 process.on("uncaughtException", (err) => {
-    console.log("Uncaught Exception detected... Server shutting down..", err);
+  console.log("Uncaught Exception detected... Server shutting down..", err);
 
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
-})
+  process.exit(1);
+});
 
 // Unhandler rejection error
 // Promise.reject(new Error("I forgot to catch this promise"))
@@ -84,10 +82,10 @@ process.on("uncaughtException", (err) => {
 // Uncaught Exception Error
 // throw new Error("I forgot to handle this local erro")
 
-
 /**
  * unhandled rejection error
  * uncaught rejection error
  * signal termination sigterm
  */
 
+export default app;
